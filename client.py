85c05d5297c71195438e0b1cc2d49dfdc494039b
Gabriel Gonçalves <ggoncalves@opensoft.pt>
2022-08-23 19:25:28 +0100

    make it better

--- client.py
+++ client.py
@@ -1,18 +1,47 @@
+import queue
 import socketio
 import pyttsx3
+from queue import Queue
+from threading import Thread
+import time
 
+q = Queue()
+
+def say_loop():
+    engine = pyttsx3.init()
+    engine.setProperty('voice', engine.getProperty('voices')[1].id)
+    engine.setProperty('rate',150)
+    while True:
+        item = q.get()
+        print(item)
+        engine.say(item)
+        engine.runAndWait()
+        q.task_done()
+
+t = Thread(target=say_loop)
+t.daemon = True
+t.start()
 sio = socketio.Client()
-engine = pyttsx3.init()
+number_registered = False
+
 
 @sio.event
 def connect():
-    print("I'm connected!")
-    sio.emit('message', "hello")
+    sio.emit('register_number', "hello")
 
 @sio.on('message')
 def got_message(message):
-    print(message)
-    engine.say("I will speak this text")
-    engine.runAndWait()
+    if number_registered:
+        q.put(message)
 
+@sio.on('register_number_response')
+def register_number_response(resp):
+    if(resp == "Success"):
+        q.put(f"Connected to race control as car number {car_number}")
+    else:   
+        q.put(f"Car number {car_number} already registered, shutting down")
+        time.sleep(3)
+        exit()
+
+car_number = input("Please insert your car number and press enter:\n")
 sio.connect("https://rocky-reaches-49584.herokuapp.com")
--- server.py
+++ server.py
@@ -6,18 +6,30 @@ sio = socketio.AsyncServer()
 app = web.Application()
 sio.attach(app)
 
+#dict instance containing the car numbers as keys and the socket ids as values
+socket_dict = {}
+
+
+@sio.on('register_number')
+def register_number(sid, number):
+    car_number = int(number)
+    if car_number in dict:
+        sio.emit('register_number_response', "Failure")
+    else:
+        socket_dict[int(number)] = sid
+        sio.emit('register_number_response', "Success")
+
 @sio.on('message')
 def print_message(sid, message):
     print(sid, message) 
 
 @sio.event
 async def connect(sid, environ, auth):
-    await sio.emit('message', "Drive through penalty for car number 69 420")
+    #await sio.emit('message', "Drive through penalty for car number 69 420")
     print('connect ', sid)
 
 @sio.event
 async def disconnect(sid):
     print('disconnect ', sid)
-
 
 web.run_app(app, port=os.environ.get('PORT'))
